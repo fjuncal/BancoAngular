2697afc2bcd6f9073773e8c5443ae1fe52413d01
fjuncal <fjuncal7@gmail.com>
2021-07-19 23:15:49 -0300

    primeiro committtttt

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { TransferenciaService } from './services/transferencia.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bytebank';
+  // destino!: number;
+  // valor!: number;
+
+  constructor(private service: TransferenciaService){
+
+  }
+
+}
